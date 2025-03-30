ad315f4f351bc574570557b29e2a3c0898bfc103
Toby McGowan <cyberiexinternational@gmail.com>
2025-03-30 14:23:40 +0100

    Finally got a basic Hunchentoot dynamic server thing up and running

--- lisp-web-server.lisp
+++ lisp-web-server.lisp
@@ -1,5 +1,5 @@
 ;Load modules
-(ql:quickload '("hunchentoot" "caveman2" "spinneret"
+(ql:quickload '("hunchentoot" "caveman2" "cl-who" "spinneret"
                 "djula" "easy-routes"))
 
 ;Set up acceptor and start listening on 4242..
@@ -10,3 +10,33 @@
 ;Set another directory to serve pages from...
 (setf (hunchentoot:acceptor-document-root *acceptor*)
       #p"/home/cryptao/Development/Conexus/www")
+
+
+;;Example of using Hunchentoot and cl-who to generate content
+
+(defpackage :webserver
+  (:use :common-lisp :hunchentoot :cl-who))
+
+(in-package :webserver)
+
+(setf *dispatch-table*
+      (list #'dispatch-easy-handlers
+            #'default-dispatcher))
+
+(setf *show-lisp-errors-p* t
+      *show-lisp-backtraces-p* t)
+
+(define-easy-handler (easy-demo :uri "/lisp/hello"
+                                :default-request-type :get)
+    ((state-variable :parameter-type 'string))
+  (with-html-output-to-string (*standard-output* nil :prologue t)
+    (:html
+     (:head (:title "Hello, world!"))
+     (:body
+      (:h1 "Hello, world!")
+      (:p "This is my Lisp web server, running on Hunchentoot,"
+          " as described in "
+          (:a :href
+              "http://newartisans.com/blog_files/hunchentoot.primer.php"
+              "this blog entry")
+          " on Common Lisp and Hunchentoot.")))))
